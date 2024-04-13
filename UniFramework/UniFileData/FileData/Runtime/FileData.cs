aef481db741f151aaf6d60a918551971298e5136
mov <34427867+fengbinmov@users.noreply.github.com>
2024-04-13 16:07:42 +0800

    update filedata

---
+++ UniFramework/UniFileData/FileData/Runtime/FileData.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace UniFramework.FileData
+{
+    /// <summary>
+    /// FileBase 将指定类型本地化压缩存储与读取
+    /// </summary>
+    [Serializable]
+    public abstract class FileBase<T>
+    {
+        private string _pathRoot;
+        private bool _dirty = true;         //标记数据对象是否已更改
+
+        public string assetPath;            //文件存储路径的相对路径(相对于 PathRoot)
+
+        [SerializeField]
+        private T _data;                    //数据对象
+
+        private string SavePath => Path.Combine(PathRoot, assetPath);       //实际保存地址(PathRoot & assetPath)
+        public string PathRoot              //保存根目录
+        {
+            get {
+                if (_pathRoot == null)
+                {
+                    _pathRoot = Application.persistentDataPath;
+                }
+                return _pathRoot;
+            }
+            set {
+                if (!Directory.Exists(value))
+                {
+                    throw new Exception($"该目录不存在 {value}");
+                }
+                _pathRoot = value;
+            }
+        }
+
+        public bool Exist => File.Exists(SavePath);     //数据对象是否有本地记录
+        public T Data => _data;
+
+        /// <summary>
+        /// 检查资产路径是否有效
+        /// </summary>
+        public bool AssetPathValid(string assetPath_) => File.Exists(Path.Combine(PathRoot, assetPath_));
+
+        public void Init(string assetPath_) {
+
+            assetPath = assetPath_;
+
+            if (Exist) Read();
+            else
+            {
+                _dirty = true;
+
+                T module = Activator.CreateInstance<T>();
+                _data = module;
+            }
+        }
+
+        public void SetDirty(bool value = true)
+        {
+            _dirty = value;
+        }
+
+        public void Read(T data_)
+        {
+            _dirty = true;
+            _data = data_;
+        }
+
+        public void Read()
+        {
+            if (!Exist)
+            {
+                Debug.LogError($"FileData {typeof(T)} is Valid, file is not exist!");
+                return;
+            }
+
+            _dirty = false;
+
+            _data = LoadFromNative(SavePath);
+        }
+
+        public void Save()
+        {
+            if (!_dirty || _data == null) return;
+
+            _dirty = false;
+
+            SaveToNative(SavePath);
+        }
+
+        public virtual T LoadFromNative(string path)
+        {
+            return FileHelper.LoadFromBinary<T>(path);
+        }
+        public virtual void SaveToNative(string path)
+        {
+            FileHelper.ToBinary(_data, path);
+        }
+    }
+
+    /// <summary>
+    /// 数据对象 二进制存取
+    /// </summary>
+    [Serializable]
+    public class FileData<T> : FileBase<T>
+    {
+        public override T LoadFromNative(string path) 
+        { 
+            return FileHelper.LoadFromBinary<T>(path);
+        }
+
+        public override void SaveToNative(string path)
+        {
+            FileHelper.ToBinary(Data, path);
+        }
+    }
+        
+    /// <summary>
+    /// 数据对象 JsonText存取
+    /// </summary>
+    [Serializable]
+    public class FileJson<T> :FileBase<T>
+    {
+        public override T LoadFromNative(string path)
+        {
+            var text = FileHelper.LoadFromText(path);
+            return JsonUtility.FromJson<T>(text);
+        }
+
+        public override void SaveToNative(string path)
+        {
+            var text = JsonUtility.ToJson(Data);
+            FileHelper.ToText(text, path);
+        }
+    }
+}
---
+++ UniFramework/UniFileData/FileData/Runtime/FileHelper.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using System.IO.Compression;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace UniFramework.FileData
+{
+    /// <summary>
+    /// 文件操作过程自动压缩
+    /// </summary>
+    public class FileHelper
+    {
+        private static BinaryFormatter _binaryFormatter;
+        private static BinaryFormatter BinaryFormat
+        {
+            get
+            {
+                if (_binaryFormatter == null)
+                {
+                    SurrogateSelector surrogate = new SurrogateSelector();
+                    StreamingContext context = new StreamingContext(StreamingContextStates.All);
+                    surrogate.AddSurrogate(typeof(Vector2), context, new Vector2SerializationSurrogate());
+                    surrogate.AddSurrogate(typeof(Vector3), context, new Vector3SerializationSurrogate());
+                    surrogate.AddSurrogate(typeof(Vector4), context, new Vector4SerializationSurrogate());
+                    surrogate.AddSurrogate(typeof(Vector2Int), context, new Vector2IntSerializationSurrogate());
+                    surrogate.AddSurrogate(typeof(Vector3Int), context, new Vector3IntSerializationSurrogate());
+                    surrogate.AddSurrogate(typeof(Color), context, new ColorSerializationSurrogate());
+                    surrogate.AddSurrogate(typeof(BoneWeight), context, new BoneWeightSerializationSurrogate());
+                    surrogate.AddSurrogate(typeof(Bounds), context, new BoundsSerializationSurrogate());
+                    surrogate.AddSurrogate(typeof(Matrix4x4), context, new Matrix4x4SerializationSurrogate());
+
+                    _binaryFormatter = new BinaryFormatter(surrogate, context);
+                }
+                return _binaryFormatter;
+            }
+        }
+
+        /// <summary>
+        /// 将target 存储到指定位置
+        /// </summary>
+        /// <param name="target">目标</param>
+        /// <param name="filePath">文件地址</param>
+        /// <param name="crcCode">是否记录 该文件的Crc16CcittFalse 值（crcCode == null 则不记录）</param>
+        /// <returns>文件地址 存储失败则返回string.Empty</returns>
+        public static void ToBinary<T>(T target, string filePath)
+        {
+            MemoryStream stream = new MemoryStream();
+
+            BinaryFormat.Serialize(stream, target);
+
+            string direct = Path.GetDirectoryName(filePath);
+            if (!Directory.Exists(direct)) Directory.CreateDirectory(direct);
+
+            FileStream fileStream = File.Create(filePath);
+
+            CompressStream(stream, fileStream);
+
+            fileStream.Close();
+
+            stream.Dispose();
+
+            if (!File.Exists(filePath))
+            {
+                Debug.Log($"Failed to save data to the local directory, [filePath,{filePath}] not exist!");
+            }
+        }
+
+        /// <summary>
+        /// 将 bytes 解析为目标对象 target
+        /// </summary>
+        /// <param name="target">目标对象</param>
+        /// <param name="filePath">文件地址</param>
+        /// <param name="crcCode">是否记录 该文件的Crc16CcittFalse 值（crcCode == null 则不记录）</param>
+        /// <returns>解析成功的目标对象</returns>
+        public static T LoadFromBinary<T>(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    Stream fileStream = File.OpenRead(filePath);
+                    MemoryStream stream = new MemoryStream();
+                    DecompressStream(fileStream, stream);
+                    T target = (T)BinaryFormat.Deserialize(stream);
+                    fileStream.Close();
+
+                    stream.Dispose();
+
+                    return target;
+                }
+                catch (Exception e)
+                {
+                    Debug.Log($"Failed to read the data. The input file format is incorrect\n{e.ToString()}");
+                    return default;
+                }
+            }
+            else
+            {
+                Debug.Log($"Failed to load data to the local directory, [filePath,{filePath}] not exist!");
+                return default;
+            }
+        }
+
+        /// <summary>
+        /// 将target 存储到指定位置
+        /// </summary>
+        /// <param name="target">目标</param>
+        /// <param name="filePath">文件地址</param>
+        /// <param name="crcCode">是否记录 该文件的Crc16CcittFalse 值（crcCode == null 则不记录）</param>
+        /// <returns>文件地址 存储失败则返回string.Empty</returns>
+        public static void ToText(string text, string filePath)
+        {
+            MemoryStream stream = new MemoryStream();
+
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            stream.Write(bytes,0, bytes.Length);
+
+            string direct = Path.GetDirectoryName(filePath);
+            if (!Directory.Exists(direct)) Directory.CreateDirectory(direct);
+
+            FileStream fileStream = File.Create(filePath);
+
+            CompressStream(stream, fileStream);
+
+            fileStream.Close();
+
+            stream.Dispose();
+
+            if (!File.Exists(filePath))
+            {
+                Debug.Log($"Failed to save data to the local directory, [filePath,{filePath}] not exist!");
+            }
+        }
+
+        /// <summary>
+        /// 将 bytes 解析为目标对象 target
+        /// </summary>
+        /// <param name="target">目标对象</param>
+        /// <param name="filePath">文件地址</param>
+        /// <param name="crcCode">是否记录 该文件的Crc16CcittFalse 值（crcCode == null 则不记录）</param>
+        /// <returns>解析成功的目标对象</returns>
+        public static string LoadFromText(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    Stream fileStream = File.OpenRead(filePath);
+                    MemoryStream stream = new MemoryStream();
+                    DecompressStream(fileStream, stream);
+
+                    string text = Encoding.UTF8.GetString(stream.GetBuffer());
+
+                    fileStream.Close();
+
+                    stream.Dispose();
+
+                    return text;
+                }
+                catch (Exception e)
+                {
+                    Debug.Log($"Failed to read the data. The input file format is incorrect\n{e.ToString()}");
+                    return default;
+                }
+            }
+            else
+            {
+                Debug.Log($"Failed to load data to the local directory, [filePath,{filePath}] not exist!");
+                return default;
+            }
+        }
+
+        internal static void CompressStream(Stream sources, Stream target)
+        {
+            GZipStream compressionStream = new GZipStream(target, CompressionMode.Compress);
+            sources.Seek(0, SeekOrigin.Begin);
+            sources.CopyTo(compressionStream);
+            compressionStream.Close();
+        }
+
+        internal static void DecompressStream(Stream sources, Stream target)
+        {
+            var zipStream = new GZipStream(sources, CompressionMode.Decompress);
+            zipStream.CopyTo(target);
+            target.Seek(0, SeekOrigin.Begin);
+            zipStream.Close();
+        }
+    }
+}
